4e516564361dd86323a7f6c269e66fcdcb0ecc21
alex4nda <ubaezuonuchidera5@gmail.com>
2024-06-12 11:08:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.scss'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import logo from "./image/logo.svg"
+
+document.querySelector('#app').innerHTML = `
+
+
+
+
+`
+
+
